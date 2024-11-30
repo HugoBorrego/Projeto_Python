111c8b0a38bf47a4681801471e341b578a006282
Hugo <hugoborrego02@gmail.com>
2024-11-29 23:23:43 -0300

    Projeto de Python

---
+++ projeto.py
@@ -0,0 +1,25 @@
+#Programa que organiza qualquer pasta do PC 
+
+import os
+from tkinter.filedialog import askdirectory
+
+caminho = askdirectory(title="Selecione uma pasta")
+
+lista_arquivos = os.listdir(caminho)
+
+locais = {
+    "imagens": [".png", ".jpg"],
+    "planilhas": [".xlsx", ".xls"],
+    "pdfs": [".pdf"],
+    "csv": [".csv"],
+    "word": [".css", ".doc", ".docx"],
+    "powerpoint": [".pptx"]
+}
+
+for arquivo in lista_arquivos:
+    nome, extensao = os.path.splitext(f"{caminho}/{arquivo}")
+    for pasta in locais:
+        if extensao in locais[pasta]:
+            if not os.path.exists(f"{caminho}/{pasta}"):
+                os.mkdir(f"{caminho}/{pasta}")
+            os.rename(f"{caminho}/{arquivo}", f"{caminho}/{pasta}/{arquivo}")
